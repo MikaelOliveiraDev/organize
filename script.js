adbeefdb0ba8af17361bd404b01baa9b6b6755a2
MikaelOliveiraDev <mikaeloliveiradev@gmail.com>
2024-02-20 08:52:45 -0300

    Add user strict in script

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var DIV_textResult = document.querySelector("div.textResult");
+"user strict"
+
+let DIV_textResult = document.querySelector("div.textResult");
 
 function createVerse(leftText, rightText) {
-  var DIV_verse = document.createElement("div");
-  var DIV_left = document.createElement("div");
-  var DIV_right = document.createElement("div");
+  let DIV_verse = document.createElement("div");
+  let DIV_left = document.createElement("div");
+  let DIV_right = document.createElement("div");
 
   DIV_verse.classList.add("verse");
   DIV_left.classList.add("left");
@@ -16,10 +18,10 @@ function createVerse(leftText, rightText) {
   DIV_textResult.appendChild(DIV_verse);
 }
 function createAllVerses(leftText, rightText) {
-  var left = leftText.split("\n");
-  var right = rightText.split("\n");
+  let left = leftText.split("\n");
+  let right = rightText.split("\n");
 
-  for (var i = 0; i < left.length || i < right.length; i++) {
+  for (let i = 0; i < left.length || i < right.length; i++) {
     leftText = left[i] || "";
     rightText = right[i] || "";
     createVerse(leftText, rightText);
@@ -30,23 +32,23 @@ function clearVerses() {
 }
 
 function organize() {
-  var TEXTAREA_left = document.querySelector("textarea.left");
-  var TEXTAREA_right = document.querySelector("textarea.right");
-  var left = TEXTAREA_left.value;
-  var right = TEXTAREA_right.value;
+  let TEXTAREA_left = document.querySelector("textarea.left");
+  let TEXTAREA_right = document.querySelector("textarea.right");
+  let left = TEXTAREA_left.value;
+  let right = TEXTAREA_right.value;
 
   clearVerses();
   createAllVerses(left, right);
 }
-var BUTTON_organize = document.querySelector("button");
+let BUTTON_organize = document.querySelector("button");
 BUTTON_organize.addEventListener("click", organize);
 
 function center() {
-  var aDIV_left = document.querySelectorAll("div.left");
+  let aDIV_left = document.querySelectorAll("div.left");
   aDIV_left.forEach(function (v) {
     v.classList.toggle("align-right");
     console.log(82726626);
   });
 }
-var INPUT_center = document.querySelector("#center");
+let INPUT_center = document.querySelector("#center");
 INPUT_center.addEventListener("click", center);
